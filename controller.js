4fe4b63351909470a080b223474a0b070db77236
icekid35 <bellohabib682@gmail.com>
2023-07-23 10:53:12 +0100

    modified tweets

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path, { dirname } from "path";
-import modifiedTweets from "./modified-tweets.json" assert { type: "json" };
+import * as modifiedTweets from "./tweets.json" assert { type: "json" };
 import { TwitterApi } from "twitter-api-v2";
 import { config } from "dotenv";
 config();
